13a6437f127b8ba75a0b4bb32f8b6e07002bd7f3
Popeydanielmate <14269@coderacademy.edu.au>
2024-09-01 15:48:53 +1000

    feat: App.jsx updated to handle pop up login

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom"; 
 import HomePage from "./pages/HomePage";
 import MyCollectionPage from "./pages/MyCollectionPage";
 import Template from "./pages/_TemplatePage";
 import VerifyEmail from "./pages/VerifyEmail";
+import Header from "./components/Header";
+import PopUpLogin from "./components/PopUpLogin"; 
 import './styles/index.css';
 
 function App() {
   const [token, setToken] = React.useState(localStorage.getItem('token'));
+  const [isPopUpVisible, setIsPopUpVisible] = React.useState(false); 
+  const navigate = useNavigate(); 
 
   React.useEffect(() => {
     console.log('App loaded. Token:', token);
   }, [token]);
 
+  const handleCollectionClick = () => {
+    if (!token) {
+      setIsPopUpVisible(true); 
+    } else {
+      navigate('/collection'); 
+    }
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Template />}>
-        <Route index element={<HomePage token={token} setToken={setToken} />} />
-        <Route path="verify-email" element={<VerifyEmail />} />
-        <Route path="collection" element={<MyCollectionPage token={token} setToken={setToken} />} />
-      </Route>
-    </Routes>
+    <>
+      <Header token={token} handleCollectionClick={handleCollectionClick} /> 
+      {isPopUpVisible && (
+        <PopUpLogin
+          onClose={() => setIsPopUpVisible(false)}
+          onLogin={(newToken) => {
+            setToken(newToken);
+            setIsPopUpVisible(false);
+            navigate('/collection');
+          }}
+        />
+      )}
+      <Routes>
+        <Route path="/" element={<Template />}>
+          <Route index element={<HomePage token={token} setToken={setToken} />} />
+          <Route path="verify-email" element={<VerifyEmail />} />
+          <Route path="collection" element={<MyCollectionPage token={token} setToken={setToken} />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
