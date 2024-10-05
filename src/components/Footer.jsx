776af831605d9627ae9c92a7fe7578242b705b41
Popeydanielmate <14269@coderacademy.edu.au>
2024-10-05 16:46:20 +1000

    fix: wording on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
 	return (
 		<footer>
 			<h6>Copyright 2024 Physical Media Library. Made by Daniel</h6>
-            <div className="footer-info">This site uses eBay for pricing estimation and is intended as just a guide</div>
+            <div className="footer-info">This site uses your own pricing estimation and is intended as just a guide</div>
 			<a href="mailto:physicalmediacollectors@outlook.com">Email us!</a>
 		</footer>
 	)
